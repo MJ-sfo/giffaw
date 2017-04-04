a8e60cb5002aa081ea368c61504e4338449f59b4
MJ-sfo <michaeljlaird@yahoo.com>
2017-04-04 16:52:25 -0700

    usese js and agax to hijack form

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,23 @@
 $(document).on("ready", function(){
 
-});
+  var searchForm = $('.btn-primary');  //the 'go' button
+  searchForm.on('click', hijack);
+  function hijack(event){
+    event.preventDefault();
+
+    $.ajax( {
+      method: "GET",
+      // url: "https://github.com/Giphy/GiphyAPI", // this is a "relative" link
+      url: "http://api.giphy.com/v1/gifs/trending?api_key=dc6zaTOxFJmzC" , //  from example of "Fetch GIFs currently trending online.",  "Returns 25 results by default."
+      // data: bookData,
+      dataType: "json",
+      success: onSuccess
+    });   // ajax
+
+    function onSuccess(responseData) {
+      console.log("celebrate!");
+      console.log(responseData);
+      // celebrate!
+    };  // function onSuccess
+  }   //  function hijack
+});   // document
