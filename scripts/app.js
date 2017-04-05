0cf2cbfca90346f31b5fc09cb04f77d9993a6c99
MJ-sfo <michaeljlaird@yahoo.com>
2017-04-04 21:02:22 -0700

    clearer js functions

--- scripts/app.js
+++ scripts/app.js
@@ -1,7 +1,10 @@
-$(document).on("ready", function(){
+// var giphy_api = "http://api.giphy.com/v1/gifs/search";  //  example from 'solutions' does not work on my page - get error msg:
+// 2jquery-2.2.2.min.js:4 GET http://api.giphy.com/v1/gifs/search 401 (Unauthorized)
 
+$(document).on("ready", function(){
   var searchForm = $('.btn-primary');  //the 'go' button
-  searchForm.on('click', hijack);
+  $("form").on('submit', hijack);
+  // searchForm.on('click', hijack);   // my original way
   function hijack(event){
     event.preventDefault();
     userAjax() ;
@@ -9,18 +12,19 @@ $(document).on("ready", function(){
 });   // document
 
 function userAjax() {
-    $.ajax( {
-      method: "GET",
-      // url: "https://github.com/Giphy/GiphyAPI", // this is a "relative" link
-      url: "http://api.giphy.com/v1/gifs/trending?api_key=dc6zaTOxFJmzC" , //  from example of "Fetch GIFs currently trending online.",  "Returns 25 results by default."
-      // data: bookData,
-      dataType: "json",
-      success: onSuccess
-    });   // ajax
+  $.ajax( {
+    method: "GET",
+    // url: "https://github.com/Giphy/GiphyAPI", // this is a "relative" link
+    url: "http://api.giphy.com/v1/gifs/trending?api_key=dc6zaTOxFJmzC" , //  from example of "Fetch GIFs currently trending online.",  "Returns 25 results by default."
+    // url: giphy_api,  //  example from 'solutions' - different url
+    // data: bookData,
+    dataType: "json",
+    success: onSuccess
+  });   // ajax
 
-    function onSuccess(responseData) {
-      console.log("celebrate!");
-      console.log(responseData);
-      // celebrate!
-    };  // function onSuccess
+  function onSuccess(responseData) {
+    console.log("celebrate! sucessfuly hijacked search function");
+    console.log(responseData);
+    // celebrate!
+  };  // function onSuccess
 }  // userAjax
