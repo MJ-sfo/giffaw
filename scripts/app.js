bd4406ba8bb4a6d828d3d36d257d30e7b09cc5e1
MJ-sfo <michaeljlaird@yahoo.com>
2017-04-04 22:07:14 -0700

    prints usls with gifs retured from server

--- scripts/app.js
+++ scripts/app.js
@@ -7,24 +7,34 @@ $(document).on("ready", function(){
   // searchForm.on('click', hijack);   // my original way
   function hijack(event){
     event.preventDefault();
+    // console.log( $( this ).serialize() );   // for serialization:
     userAjax() ;
   }   //  function hijack
 });   // document
 
 function userAjax() {
-  $.ajax( {
+  var xhr = $.ajax( {
     method: "GET",
     // url: "https://github.com/Giphy/GiphyAPI", // this is a "relative" link
     url: "http://api.giphy.com/v1/gifs/trending?api_key=dc6zaTOxFJmzC" , //  from example of "Fetch GIFs currently trending online.",  "Returns 25 results by default."
     // url: giphy_api,  //  example from 'solutions' - different url
     // data: bookData,
     dataType: "json",
+    // data: $("form").serialize(),    // from solutions - but not result.  documentions seems to say used data when POST to server - gives data in object format.
     success: onSuccess
   });   // ajax
+
+  xhr.done(function (data) {    // this prints out all urls with gifs
+    $.each(data.data, function () {
+        console.log(this.images.fixed_height.url);
+    })
+  });
 
   function onSuccess(responseData) {
     console.log("celebrate! sucessfuly hijacked search function");
     console.log(responseData);
     // celebrate!
+
+
   };  // function onSuccess
 }  // userAjax
